f0786da581d27533af37540ef7ba3a2b8c7fee3a
jbohnevail <88842570+jbohnevail@users.noreply.github.com>
2021-08-24 16:30:59 -0500

    Update dequeue-member.cs

--- dequeue-member.cs
+++ dequeue-member.cs
@@ -1,1 +1,31 @@
+using System;
+using com.freeclimb.api;
+using com.freeclimb.api.queue;
 
+namespace DequeueMember {
+  class Program {
+    static string getFreeClimbAccountId () {
+      return System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+    }
+
+    static string getFreeClimbApiKeys () {
+      return System.Environment.GetEnvironmentVariable("API_KEY");
+    }
+
+    static void Dequeue(string queueId, string callId = "") {
+      // Create FreeClimbClient object
+      FreeClimbClient client = new FreeClimbClient (getFreeClimbAccountId (),
+        getFreeClimbApiKeys ());
+
+      // Invoke method to update the QueueMeber to dequeue it from the queue
+      QueueMember queueMember = client.getQueuesRequester.updateQueueMember(queueId, callId);
+
+    }
+    static void Main (string[] args) {
+      string queueId = "";
+      string callId = "";
+
+      Dequeue(queueId, callId);
+    }
+  }
+}
