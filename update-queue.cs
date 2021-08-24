2804f909b91a3f7fad271a1e8426612e29632c16
jbohnevail <88842570+jbohnevail@users.noreply.github.com>
2021-08-24 16:28:25 -0500

    Update update-queue.cs

--- update-queue.cs
+++ update-queue.cs
@@ -1,1 +1,21 @@
+using System;
+using com.freeclimb.api;
+using com.freeclimb.api.queue;
 
+namespace GetQueue {
+  class Program {
+
+    static string getFreeClimbAccountId () { return System.Environment.GetEnvironmentVariable("ACCOUNT_ID"); }
+    static string getFreeClimbApiKeys () {return System.Environment.GetEnvironmentVariable("API_KEY"); }
+    static void Main (string[] args) {
+      string queueId = "";
+      // Create FreeClimbClient object
+      FreeClimbClient client = new FreeClimbClient (getFreeClimbAccountId (),
+        getFreeClimbApiKeys ());
+
+      // Invoke get method to retrieve queued metadata
+      Queue queue = client.getQueuesRequester.get (queueId);
+      Console.WriteLine(queue.getQueueId);
+    }
+  }
+}
