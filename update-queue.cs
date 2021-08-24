3b0f6a7cb78d44521ebf689d97ffc7b842098c0d
jbohnevail <88842570+jbohnevail@users.noreply.github.com>
2021-08-24 16:29:28 -0500

    Update update-queue.cs

--- update-queue.cs
+++ update-queue.cs
@@ -1,21 +1,37 @@
-using System;
 using com.freeclimb.api;
 using com.freeclimb.api.queue;
 
-namespace GetQueue {
-  class Program {
+namespace UpdateQueue
+{
+    class Program
+    {
+        public static string getFreeClimbAccountId()
+        {
+          return System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+        }
 
-    static string getFreeClimbAccountId () { return System.Environment.GetEnvironmentVariable("ACCOUNT_ID"); }
-    static string getFreeClimbApiKeys () {return System.Environment.GetEnvironmentVariable("API_KEY"); }
-    static void Main (string[] args) {
-      string queueId = "";
-      // Create FreeClimbClient object
-      FreeClimbClient client = new FreeClimbClient (getFreeClimbAccountId (),
-        getFreeClimbApiKeys ());
+        public static string getFreeClimbApiKeys()
+        {
+          return System.Environment.GetEnvironmentVariable("API_KEY");
+        }
 
-      // Invoke get method to retrieve queued metadata
-      Queue queue = client.getQueuesRequester.get (queueId);
-      Console.WriteLine(queue.getQueueId);
+        static void Main(string[] args)
+        {
+
+          string alias = "My_FC_Queue";
+          int maxSize = 10;
+          string queueId = System.Environment.GetEnvironmentVariable("QUEUE_ID");
+
+          // Create FreeClimbClient object
+          FreeClimbClient client = new FreeClimbClient(getFreeClimbAccountId(),
+                                              getFreeClimbApiKeys());
+
+          QueueOptions options = new QueueOptions();
+          options.setAlias(alias);
+          options.setMaxSize(maxSize);
+
+          // Invoke get method to modify the queue
+          Queue queue = client.getQueuesRequester.update(queueId, options);
+        }
     }
-  }
 }
