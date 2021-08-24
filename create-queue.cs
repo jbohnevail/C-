17003eaa676b62e25e4e26d11676656d718ba42f
jbohnevail <88842570+jbohnevail@users.noreply.github.com>
2021-08-24 16:26:42 -0500

    Update create-queue.cs

--- create-queue.cs
+++ create-queue.cs
@@ -1,1 +1,32 @@
+using com.freeclimb.api;
+using com.freeclimb.api.queue;
+namespace CreateQueue
+{
+  class Program
+  {
+    static string getFreeClimbAccountId()
+    {
+      return System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+    }
 
+    static string getFreeClimbApiKeys()
+    {
+      return System.Environment.GetEnvironmentVariable("API_KEY");
+    }
+
+    static void Main(string[] args)
+    {
+      string alias = "My_First_Queue";
+
+      QueueOptions options = new QueueOptions();
+      options.setAlias(alias); // Set the optional alias
+
+      // Create FreeClimbClient object
+      FreeClimbClient client = new FreeClimbClient(getFreeClimbAccountId(),
+                                           getFreeClimbApiKeys());
+
+      // Invoke method to create queue metadata
+      Queue queue = client.getQueuesRequester.create(options);
+    }
+  }
+}
