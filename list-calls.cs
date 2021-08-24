5fc369790fbdc607a97f30e2295483d8e9a378b3
jbohnevail <88842570+jbohnevail@users.noreply.github.com>
2021-08-24 16:22:12 -0500

    Update list-calls.cs

--- list-calls.cs
+++ list-calls.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Collections.Generic;
+using com.freeclimb.api;
+using com.freeclimb.api.call;
 
+namespace ListCalls {
+  class Program  {
+    static void Main (string[] args) {
+      string freeClimbAccountId = System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+      string freeClimbApiKey = System.Environment.GetEnvironmentVariable("API_KEY");
+      FreeClimbClient client = new FreeClimbClient (freeClimbAccountId, freeClimbApiKey);
+      CallList callList = client.getCallsRequester.get ();
+      if (callList.getTotalSize > 0) {
+        while (callList.getLocalSize < callList.getTotalSize) {
+          callList.loadNextPage ();
+        }
+        LinkedList<IFreeClimbCommon> commonList = callList.export ();
+        foreach (IFreeClimbCommon element in commonList) {
+          Call call = element as Call;
+          Console.WriteLine (call.getCallId);
+        }
+      }
+    }
+  }
+}
