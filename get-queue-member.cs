4cefa77dca36e3dd5f0f3ae0010e01d379dccd35
jbohnevail <88842570+jbohnevail@users.noreply.github.com>
2021-08-24 16:30:32 -0500

    Update get-queue-member.cs

--- get-queue-member.cs
+++ get-queue-member.cs
@@ -1,1 +1,28 @@
+using com.freeclimb.api;
+using com.freeclimb.api.queue;
 
+namespace GetQueueMember {
+  class Program {
+
+    public static string getFreeClimbAccountId()
+    {
+      return System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+    }
+
+    public static string getFreeClimbApiKeys()
+    {
+      return System.Environment.GetEnvironmentVariable("API_KEY");
+    }
+
+    static void Main (string[] args) {
+      string queueId = "";
+      string callId = "";
+      // Create FreeClimbClient object
+      FreeClimbClient client = new FreeClimbClient (getFreeClimbAccountId (),
+        getFreeClimbApiKeys ());
+
+      // Invoke get method to retrieve queued call metadata
+      QueueMember queueMember = client.getQueuesRequester.getQueueMember (queueId, callId);
+    }
+  }
+}
