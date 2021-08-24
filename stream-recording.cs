b1327bc56901e94b8372e9df534e40c907375465
jbohnevail <88842570+jbohnevail@users.noreply.github.com>
2021-08-24 16:37:36 -0500

    Update stream-recording.cs

--- stream-recording.cs
+++ stream-recording.cs
@@ -1,1 +1,30 @@
+using System;
+using System.IO;
+using com.freeclimb.api;
 
+namespace StreamRecording
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        string FreeClimbAccountId = System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+        string FreeClimbApiKey = System.Environment.GetEnvironmentVariable("API_KEY");
+        string RecordingId = "";
+
+        // Create FreeClimbClient object
+        FreeClimbClient client = new FreeClimbClient(FreeClimbAccountId, FreeClimbApiKey);
+
+        // Invoke get method with AudioReturn format of Stream to obtain IO stream of audio
+        Stream stream = client.getRecordingsRequester.stream(RecordingId);
+      }
+      catch (FreeClimbException ex)
+      {
+        // Exception throw upon failure
+        Console.Write(ex.Message);
+      }
+    }
+  }
+}
