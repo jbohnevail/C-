50a33898e24714946abfabd3deb5e8646d0c02b1
jbohnevail <88842570+jbohnevail@users.noreply.github.com>
2021-08-24 16:38:05 -0500

    Update delete-recording.cs

--- delete-recording.cs
+++ delete-recording.cs
@@ -1,1 +1,23 @@
+using System;
+using com.freeclimb.api;
 
+namespace DeleteRecording {
+  class Program {
+    static void Main (string[] args) {
+      try {
+        string freeClimbAccountId = System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+        string freeClimbApiKey = System.Environment.GetEnvironmentVariable("API_KEY");
+        string recordingId = "";
+
+        // Create FreeClimbClient object
+        FreeClimbClient client = new FreeClimbClient (freeClimbAccountId, freeClimbApiKey);
+
+        // Invoke deleted method to delete recording Url
+        client.getRecordingsRequester.delete (recordingId);
+      } catch (FreeClimbException ex) {
+        // Exception throw upon failure
+        System.Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
