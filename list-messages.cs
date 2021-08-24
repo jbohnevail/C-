61e470b64bc1d498ad9cacba51c2c1e13cec56a2
jbohnevail <88842570+jbohnevail@users.noreply.github.com>
2021-08-24 16:40:48 -0500

    Update list-messages.cs

--- list-messages.cs
+++ list-messages.cs
@@ -1,1 +1,48 @@
+using System;
+using System.Collections.Generic;
+using com.freeclimb.api;
+using com.freeclimb.api.message;
 
+namespace ListRecordings {
+
+  class Program {
+    public static string getFreeClimbAccountId () {
+      return System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+    }
+    public static string getFreeClimbApiKeys () {
+      return System.Environment.GetEnvironmentVariable("API_KEY");
+    }
+
+        static void Main(string[] args)
+        {
+            // Create FreeClimbClient object
+            FreeClimbClient client = new FreeClimbClient(getFreeClimbAccountId(), getFreeClimbApiKeys());
+
+            // Invoke get method to retrieve initial list of recording information
+            MessageList messageList = client.getMessagesRequester.get();
+
+            // Check if list is empty by checking total size of the list
+            if (messageList.getTotalSize > 0)
+            {
+                // retrieve all recording for server
+                while (messageList.getLocalSize < messageList.getTotalSize)
+                {
+                    messageList.loadNextPage();
+                }
+
+                // Convert current pages recording information to a linked list
+                LinkedList<IFreeClimbCommon> commonList = messageList.export();
+
+                // Loop through linked list to process recording information
+                foreach (IFreeClimbCommon element in commonList)
+                {
+                    // Cast each element to the Recording element for processing
+                    Message message = element as Message;
+
+                    // Process recording element
+                    Console.Write(message.getText);
+                }
+            }
+        }
+  }
+}
