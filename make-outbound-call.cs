90f20ae0ffff365f215b1d732e0a45435435b6b0
jbohnevail <88842570+jbohnevail@users.noreply.github.com>
2021-08-24 16:19:46 -0500

    Update make-outbound-call.cs

--- make-outbound-call.cs
+++ make-outbound-call.cs
@@ -1,1 +1,26 @@
+using com.freeclimb;
+using com.freeclimb.percl;
+using com.freeclimb.webhooks.call;
+using System;
+using Microsoft.AspNetCore.Mvc;
 
+namespace MakeOutboundCall.Controllers {
+  [Route ("connect")]
+  [ApiController]
+  public class FreeClimbController : ControllerBase {
+
+    [HttpPost]
+    public ActionResult CallConnect (CallStatusCallback freeClimbRequest) {
+        // Create an empty PerCL script container
+        PerCLScript script = new PerCLScript ();
+        Say say = new Say();
+        say.setText("You just got called by the C sharp S D K!");
+        Console.WriteLine(freeClimbRequest.getCallStatus);
+       
+        script.Add (say);
+
+        // Convert PerCL container to JSON and append to response
+        return Content (script.toJson (), "application/json");
+    }
+  }
+}
